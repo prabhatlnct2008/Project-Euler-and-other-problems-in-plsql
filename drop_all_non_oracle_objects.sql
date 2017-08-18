37f489ef15d82de88b4e8f3ef08f33fd526faa82
prabhat sharma <prabhatlnct2008@gmail.com>
2017-08-18 11:09:24 +0530

    Update drop_all_non_oracle_objects.sql
    This one will data files as well . To be run after setting oracle_sid and from db server .

--- drop_all_non_oracle_objects.sql
+++ drop_all_non_oracle_objects.sql
@@ -1,7 +1,40 @@
-declare
+accept  sysuser prompt  'Enter the sys user name : '
+accept   syspwd prompt  'Enter the sys user password : '  hide
 
+Prompt connecting to &sysuser.@dbtns.
+
+conn &sysuser./&syspwd as sysdba
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+
+spool deletion.log
+
+SET SERVEROUTPUT ON SIZE 1000000
+SET FEEDBACK OFF
+SET TERMOUT ON
+
+begin
+execute immediate 'create table datafiles_loc
+(file_name varchar2(30) , file_loc varchar2(1000))' ;
+exception when others then
+	dbms_output.put_line(sqlerrm) ;
+end;
+/
+
+declare
 type t_syns is table of varchar2(30) ;
 t_pub_syns t_syns := t_syns() ;
+
+Procedure execute_immediate(sql_text varchar2)
+is
+begin
+
+  dbms_output.put_line(sql_text) ;
+  execute immediate(sql_text);
+  exception when others then
+	dbms_output.put_line('Error while executing '||chr(10)||sql_text||chr(10)||sqlerrm) ;
+end;
+
 
 procedure drop_psyn 
 is 
@@ -10,7 +43,7 @@ for i in (select synonym_name from all_synonyms where owner='PUBLIC' and
                     table_owner in (select username from all_users where oracle_maintained = 'N')) loop
                     null;
   begin                    
-  execute immediate 'drop public synonym '||i.synonym_name ;
+  execute_immediate ('drop public synonym '||i.synonym_name) ;
   exception when others then
   t_pub_syns.extend(1) ;
   t_pub_syns(t_pub_syns.last) := i.synonym_name;
@@ -21,14 +54,14 @@ end drop_psyn;
 procedure drop_users is
 begin
 for i in (select username from all_users where oracle_maintained = 'N') loop
-  execute immediate 'drop user '||i.username||' cascade ';
+  execute_immediate ('drop user '||i.username||' cascade ');
 end loop;
 end;
 
 procedure drop_tbs is
 begin
 for i in (select tablespace_name from dba_tablespaces where tablespace_name not in ('SYSTEM' , 'SYSAUX' , 'UNDOTBS1' , 'TEMP' , 'USERS')) loop
-  execute immediate 'drop tablespace '||i.tablespace_name||' including contents and datafiles ' ;
+  execute_immediate ('drop tablespace '||i.tablespace_name||' including contents and datafiles ') ;
 end loop;
 end ;
 
@@ -36,12 +69,26 @@ procedure disconnect_sessions
 is
 begin
 for i in (select sid,serial# from v$session where username  in (select username from all_users where oracle_maintained = 'N')) loop
-  execute immediate 'alter system '''||i.sid||','||i.serial#||'''';
+  execute_immediate ('alter system  kill session '''||i.sid||','||i.serial#||'''');
 end loop;
 dbms_lock.sleep(10) ;
 end ;
 
+
+
 begin
+
+  dbms_output.put_line('****Backing up Data files location****') ;
+  for i in (select regexp_substr(file_name , '[^\\\/\s]+.DBF')  as dbf_file,
+				   regexp_substr(file_name , '^(^[a-zA-Z]\S+)[\\\/]'  ) as file_loc
+				from dba_data_files
+				where tablespace_name in
+						(select tablespace_name from dba_tablespaces where tablespace_name not in ('SYSTEM' , 'SYSAUX' , 'UNDOTBS1' , 'TEMP' , 'USERS'))) loop
+
+	execute immediate q'!insert into datafiles_loc values ('!'||i.dbf_file||q'!','!'||i.file_loc||q'!')!' ;
+ end loop ;
+ commit ;
+
 
   dbms_output.put_line('****DISCONNECTING CONNECTED SESSIONS****') ;
   disconnect_sessions ;
@@ -53,7 +100,56 @@ begin
   drop_tbs ;
   
   DBMS_OUTPUT.PUT_LINE('****DROPPING REMAINING PUBLIC SYNONYMS****') ;
-  FOR I IN T_PUB_SYNS.FIRST .. T_PUB_SYNS.LAST LOOP
-    execute immediate 'drop public synonym '||T_PUB_SYNS(i) ;
+  if T_PUB_SYNS.count > 0 then
+    FOR I IN T_PUB_SYNS.FIRST .. T_PUB_SYNS.LAST LOOP
+      execute immediate 'drop public synonym '||T_PUB_SYNS(i) ;
+    end loop ;
+  end if ;
+  
+  DBMS_OUTPUT.PUT_LINE('****DROPPING NON ORACLE ROLES****') ;
+  for i in (select role as role_name  from dba_roles where oracle_maintained= 'N') loop
+	execute immediate 'drop role '||i.role_name ;
   end loop ;
+  
+  DBMS_OUTPUT.PUT_LINE('****DROPPING NON ORACLE CONTEXTS****') ;
+  for i in (select namespace from dba_context where schema in (select username from dba_users where oracle_maintained = 'N')) loop
+	execute immediate 'drop context '||i.namespace ;
+  end loop ;
+
+
+  
+  exception when others then 
+   dbms_output.put_line(sqlerrm) ;
 end ;
+/
+
+Prompt Shutting Down and starting up instance 
+
+shutdown abort ;
+startup
+
+Prompt Deleting db files from server  in case not deleted earlier , also deleting non oracle roles
+
+
+
+
+begin
+for i in (select file_loc , file_name from datafiles_loc) loop
+	execute immediate 'create or replace directory db_drop_dbf '||q'! as '!'||i.file_loc||q'!'!' ;
+	begin
+	utl_file.fremove('DB_DROP_DBF' , i.file_name) ;
+	exception when others then 
+		dbms_output.put_line('Error in deleting file '||i.file_name||chr(10)||sqlerrm) ;
+    end ;
+end loop ;
+execute immediate 'drop table datafiles_loc' ;
+end;
+/
+
+ 
+
+Prompt Process is complete  . Please check logs and take necessary actions if deletion is not succesul at any level 
+
+spool off;
+
+pause prompt press enter to exit
